f5d0ec06ef93678a54383f23fb40c48f5a8d6437
JonathanCarey13 <jonathan.carey1995@gmail.com>
2021-05-12 00:50:05 -0400

    created movement

---
+++ Assets/Scripts/MouseMovementBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovementBehavior : MonoBehaviour
+{
+    public float sensitivity = 1.5f;
+    public float smoothing = 1.5f;
+
+    private float xMousePos;
+    private float smoothedMousePos;
+
+    private float currentLookingPos;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        GetInput();
+        ModifyInput();
+        MovePlayer();
+    }
+
+    void GetInput()
+    {
+        xMousePos = Input.GetAxisRaw("Mouse X");
+    }
+
+    void ModifyInput()
+    {
+        xMousePos *= sensitivity * smoothing;
+        smoothedMousePos = Mathf.Lerp(smoothedMousePos, xMousePos, 1f / smoothing);
+    }
+
+    void MovePlayer()
+    {
+        currentLookingPos += smoothedMousePos;
+        transform.localRotation = Quaternion.AngleAxis(currentLookingPos, transform.up);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehavior : MonoBehaviour
+{
+    public float playerSpeed = 20f;
+    private CharacterController characterController;
+    public Animator cameraAnimation;
+    private bool isWalking;
+
+    private Vector3 inputVector;
+    private Vector3 movementVector;
+    private float gravity = -10f;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        GetInput();
+        MovePlayer();
+        CheckForHeadBob();
+
+        cameraAnimation.SetBool("isWalking", isWalking);
+    }
+
+    void GetInput()
+    {
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        inputVector.Normalize();
+        inputVector = transform.TransformDirection(inputVector);
+
+        movementVector = (inputVector * playerSpeed) + (Vector3.up * gravity);
+    }
+
+    void MovePlayer()
+    {
+        characterController.Move(movementVector * Time.deltaTime);
+    }
+
+    void CheckForHeadBob()
+    {
+        if (characterController.velocity.magnitude > .1f)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+}
